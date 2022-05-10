b66dad0c2274ef73941ec4d78e1c1cc1716b86e4
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2022-05-10 21:48:29 +0200

    Botonera Mejorada

---
+++ js.js
@@ -0,0 +1,20 @@
+var rosaOriginal = "rgb(239, 71, 111)";
+
+function colorearRosa(boton){
+    boton.style.color = rosaOriginal;
+  }
+  
+  function colorearBlanco(boton){
+    boton.style.color = "White";
+  }
+
+  function colorearInverso(boton){
+    boton.style.color = rosaOriginal;
+    boton.style.backgroundColor = "White";
+  }
+
+  function colorearNormal(boton){
+
+    boton.style.color = "White";
+    boton.style.backgroundColor = rosaOriginal;
+  }
