e2debb1508429f4bdb7de2373b746966f08079f7
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2022-05-20 19:45:42 +0200

    Responsive

--- js.js
+++ js.js
@@ -3,8 +3,6 @@ var rosaOriginal = "rgb(239, 71, 111)";
 var w = window.innerWidth;
 var h = window.innerHeight;
 
-alert(w);
-alert(h);
 
 function colorearRosa(boton){
     boton.style.color = rosaOriginal;
